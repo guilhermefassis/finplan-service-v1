f2444eaf1757497a7576fe91bb58cd00c42c09bf
Guilherme Assis <guilhermefassis-dev@outlook.com>
2026-01-05 15:51:34 -0300

    Create spring boot repository

---
+++ backend/finplan/src/main/java/io/finplan/FinplanApplication.java
@@ -0,0 +1,13 @@
+package io.finplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinplanApplication.class, args);
+	}
+
+}
---
+++ backend/finplan/src/test/java/io/finplan/FinplanApplicationTests.java
@@ -0,0 +1,13 @@
+package io.finplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
