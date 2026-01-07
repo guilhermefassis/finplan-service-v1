fc9a3ba1445d65849dc57f4ccead5f9cccab8e55
Guilherme Assis <guilhermefassis-dev@outlook.com>
2026-01-07 18:40:41 -0300

    Backend: Add user entity

---
+++ backend/finplan/src/main/java/io/finplan/domain/entity/User.java
@@ -0,0 +1,50 @@
+package io.finplan.domain.entity;
+
+import io.finplan.domain.model.enums.PaymentFrequencyEnum;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.type.SqlTypes;
+
+import java.time.OffsetDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "payment_frequency", nullable = false, length = 20)
+    private PaymentFrequencyEnum paymentFrequency;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(name = "payment_details", nullable = false, columnDefinition = "jsonb")
+    @Builder.Default
+    private Map<String, Object> paymentDetails = new HashMap<>();
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "created_at", nullable = false)
+    private OffsetDateTime updatedAt;
+}
