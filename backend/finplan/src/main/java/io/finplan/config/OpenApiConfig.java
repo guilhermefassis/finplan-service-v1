7c3c0e323b6897fde0c95001309f70b55302bc8a
Guilherme Assis <guilhermefassis-dev@outlook.com>
2026-01-26 17:57:03 -0300

    Backend: Add OpenApi Config to configure api auth

---
+++ backend/finplan/src/main/java/io/finplan/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package io.finplan.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Financial Planning API",
+                version = "1.0.0",
+                description = "First version of the personal financial planning API."
+        ),
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
